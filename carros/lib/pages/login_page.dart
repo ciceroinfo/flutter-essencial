d5550d935cb312f3fd5040bffc2f6b1daef77a09
cicero.fabio <cicero.fabio@mjv.com.br>
2021-02-03 18:20:23 -0300

    Aula 86

--- carros/lib/pages/login_page.dart
+++ carros/lib/pages/login_page.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final _tLogin = TextEditingController();
+
   final _tSenha = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  final _focusSenha = FocusNode();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +31,35 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: [
-          _textFormField("Login", "Digite o login", controller: _tLogin),
-          SizedBox(height: 10),
-          _textFormField("Senha", "Digite a senha",
-              password: true, controller: _tSenha),
-          SizedBox(height: 20),
-          _button("Login", _onClickLogin)
-        ],
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            _textFormField(
+              "Login",
+              "Digite o login",
+              controller: _tLogin,
+              validator: _validateLogin,
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              nextFocus: _focusSenha,
+            ),
+            SizedBox(height: 10),
+            _textFormField(
+              "Senha",
+              "Digite a senha",
+              password: true,
+              controller: _tSenha,
+              validator: _validateSenha,
+              keyboardType: TextInputType.number,
+              focusNode: _focusSenha,
+            ),
+            SizedBox(height: 20),
+            _button("Login", _onClickLogin)
+          ],
+        ),
       ),
     );
   }
@@ -44,9 +78,27 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  TextFormField _textFormField(String label, String hint,
-      {bool password = false, TextEditingController controller}) {
+  TextFormField _textFormField(
+    String label,
+    String hint, {
+    bool password = false,
+    TextEditingController controller,
+    FormFieldValidator<String> validator,
+    TextInputType keyboardType,
+    TextInputAction textInputAction,
+    FocusNode focusNode,
+    FocusNode nextFocus,
+  }) {
     return TextFormField(
+      validator: validator,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      onFieldSubmitted: (String text) {
+        if (nextFocus != null) {
+          FocusScope.of(context).requestFocus(nextFocus);
+        }
+      },
       controller: controller,
       style: TextStyle(fontSize: 25, color: Colors.blue),
       decoration: InputDecoration(
@@ -60,9 +112,39 @@ class LoginPage extends StatelessWidget {
   }
 
   void _onClickLogin() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
     String l = _tLogin.text;
     String s = _tSenha.text;
 
     print("Login: $l, senha: $s");
+  }
+
+  String _validateLogin(String value) {
+    if (value.isEmpty) {
+      return "Digite o login";
+    }
+
+    return null;
+  }
+
+  String _validateSenha(String value) {
+    if (value.isEmpty) {
+      return "Digite a senha";
+    }
+
+    if (value.length < 3) {
+      return "A senha deve conter no minímo 3 digitos";
+    }
+
+    return null;
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
   }
 }
